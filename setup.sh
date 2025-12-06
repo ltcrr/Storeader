2e22a826fef65b436aa2d6e777549eabfdf46ba0
ltcwr <ltcrr@tutamail.com>
2025-12-06 23:39:42 +0100

    updated setup script
    added foundry installation

--- setup.sh
+++ setup.sh
@@ -2,13 +2,21 @@
 sudo rm -f /usr/local/bin/storeader
 
 echo "Installing storeader..."
-
 if ! command -v cast >/dev/null 2>&1; then
-    echo "Foundry is not installed"
-    exit 1
+    echo "installing foundry"
+    curl -L https://foundry.paradigm.xyz | bash
+    if [ -f "$HOME/.foundry/bin/foundryup" ]; then
+        source "$HOME/.foundry/bin/foundryup"
+        foundryup
+    else
+        echo "error while running foundryup"
+        echo "please open a new terminal and run : foundryup"
+    fi
 fi
+
+echo "successful installation"
 
 sudo cp storeader /usr/local/bin/storeader
 sudo chmod +x /usr/local/bin/storeader
 
-echo "storeader installed. Run: storeader"
+echo "done ! you can now use storeader by running : storeader"
